85d4e0dddb0698f11b36642d8fb38797407f1132
Matt Pruitt <matt@guitsaru.com>
2021-01-06 12:28:55 +0700

    Initial commit

---
+++ lib/draco/scenes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Draco
+  module Scenes
+    class Error < StandardError; end
+
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/draco/scenes_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Draco::Scenes do
+  it "has a version number" do
+    expect(Draco::Scenes::VERSION).not_to be nil
+  end
+end
